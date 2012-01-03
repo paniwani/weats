f6f53d903b5c071226695a25c1775b889c118cf2
= <=>
2012-01-03 03:14:33 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weats::Application.config.secret_token = 'd4a2db701aa0096673aeb6a4804942f08cb34215eaf7eeec4eb9e7a4938aa6cac27b62f856fc687c39b8665a130195dca1a6d33518881e66a8f761904afa1de1'
